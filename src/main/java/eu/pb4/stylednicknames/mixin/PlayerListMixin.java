2edc6dfb7f6dbca4dd6635f75dd9b19eb641e30f
Howie <howie0715@gmail.com>
2026-02-10 16:16:57 +0800

    Implement permission check for styled nicknames usage

--- src/main/java/eu/pb4/stylednicknames/mixin/PlayerListMixin.java
+++ src/main/java/eu/pb4/stylednicknames/mixin/PlayerListMixin.java
@@ -1,6 +1,7 @@
 package eu.pb4.stylednicknames.mixin;
 
 import eu.pb4.stylednicknames.NicknameHolder;
+import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.minecraft.network.Connection;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.CommonListenerCookie;
@@ -15,5 +16,8 @@ public class PlayerListMixin {
     @Inject(method = "placeNewPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/PlayerList;sendPlayerPermissionLevel(Lnet/minecraft/server/level/ServerPlayer;)V", shift = At.Shift.AFTER))
     private void styledNicknames$loadNickname(Connection connection, ServerPlayer player, CommonListenerCookie clientData, CallbackInfo ci) {
         NicknameHolder.of(player).styledNicknames$loadData();
+        if (!Permissions.check(player,"stylednicknames.use")){
+            NicknameHolder.of(player).styledNicknames$set(null, false);
+        }
     }
 }
